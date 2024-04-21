409164af490fb68202ab89e2366754a4e1c172bc
Ali Volkan <avolkan99@gmail.com>
2024-04-21 22:17:34 +0000

    up

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # custom startup script for heroku
 #!/bin/sh
+cd server && gunicorn run:app &
 
-cd server && gunicorn run:app -b 0.0.0.0:$PORT
 # Install required packages from requirements.txt
 echo "Installing required packages..."
 pip install -r requirements.txt
